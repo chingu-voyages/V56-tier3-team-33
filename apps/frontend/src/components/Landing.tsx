fc87c8c2e034e5d5eabb4414b3205bb7c0d16790
MohaD <DMRgit@outlook.fr>
2025-08-09 18:03:10 +0200

    create landing page of the project

--- apps/frontend/src/components/Landing.tsx
+++ apps/frontend/src/components/Landing.tsx
@@ -1,8 +1,10 @@
-import React from "react";
 import Nav from "./Nav";
 import styles from "../assets/landing.module.css";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import image from "../assets/hero 1.png";
+import Filterexpert from "./Filterexpert";
+import image2 from "../assets/Doctor Invite Photo 1.png";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
@@ -26,6 +28,9 @@ const Landing = (props: Props) => {
             <p>professionals based on your needs,</p>
             <p>location, and preferences.</p>
           </div>
+          <div style={{ marginTop: "100px" }}>
+            <Filterexpert />
+          </div>
         </div>
 
         <div className={styles.containerImage}>
@@ -34,6 +39,39 @@ const Landing = (props: Props) => {
             alt="This is a picture of doctors and patients"
             className={styles.imageProject}
           />
+        </div>
+      </div>
+      <div className={styles.containerImageFilter}>
+        <div
+          className={styles.containerFilter}
+          style={{ display: "flex", justifyContent: "end" }}
+        >
+          <img src={image2} alt="Picture of on doctor smiling to us" />
+        </div>
+        <div className={styles.containertext2}>
+          <p>Are you a medical professional?</p>
+          <p>share your expertise with new</p>
+          <p>clients today</p>
+          <div className={styles.containertext3}>
+            <p>Join thousands of medical professional</p>
+            <p>who trusted Wecare to connect them with</p>
+            <p>nearby patients in need of your expertise</p>
+          </div>
+          <Link
+            to="/signup"
+            style={{
+              display: "inline-block",
+              padding: "26px 36px",
+              backgroundColor: "#8DB5D9",
+              color: "white",
+              textDecoration: "none",
+              borderRadius: "4px",
+              fontSize: "22px",
+              fontWeight: 1000,
+            }}
+          >
+            join as expert
+          </Link>
         </div>
       </div>
     </div>
