303104447cd12f51f80e629e8057afaf7a9c2372
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-08-12 04:26:49 +0100

    feat(backend): create experts endpoint

---
+++ apps/backend/src/experts/experts-handlers.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function getExpertDetails(req: Request, res: Response) {
+  // normalize
+  // validate
+  // do stuff
+}
