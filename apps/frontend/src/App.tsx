aa55c169ad1ba333014c73132df02813796067f5
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-08-15 07:50:40 +0100

    feat(auth): when logged in, restrict access to auth routes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import Landing from "./components/Landing";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignupProfessional from "./components/SignupProfessional";
 import Expertlist from "./components/Expertlist";
-import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
+
+import "./App.css";
 
 export default function App() {
   return (
@@ -13,12 +20,24 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignupProfessional />} />
+            <Route element={<GuestRoutes />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignupProfessional />} />
+            </Route>
             <Route path="/result" element={<Expertlist />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
     </div>
   );
+}
+
+function GuestRoutes() {
+  const { user } = useAuth();
+
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
 }
